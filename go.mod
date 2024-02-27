e5fe309a113c8ac459c3824bd5392274bccc6bab
Igor Crevar <crewce@gmail.com>
2024-02-27 13:25:21 +0100

    oracle example

--- go.mod
+++ go.mod
@@ -2,4 +2,22 @@ module github.com/Ethernal-Tech/apex-bridge
 
 go 1.20
 
-require github.com/Ethernal-Tech/cardano-infrastructure v0.0.0-20240227111146-562a11bbc635 // indirect
+require (
+	github.com/Ethernal-Tech/cardano-infrastructure v0.0.0-20240227111146-562a11bbc635
+	github.com/hashicorp/go-hclog v1.6.2
+)
+
+require (
+	github.com/blinklabs-io/gouroboros v0.71.0 // indirect
+	github.com/boltdb/bolt v1.3.1 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/fxamacker/cbor/v2 v2.6.0 // indirect
+	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
+	github.com/jinzhu/copier v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+)
---
+++ oracle/cmd/main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"math"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/Ethernal-Tech/cardano-infrastructure/indexer"
+	db "github.com/Ethernal-Tech/cardano-infrastructure/indexer/db"
+	"github.com/Ethernal-Tech/cardano-infrastructure/logger"
+	"github.com/hashicorp/go-hclog"
+)
+
+func main() {
+	networkMagic := uint32(42)
+	address := "localhost:3000"   // "/tmp/cardano-133064331/node-spo1/node.sock"
+	startBlockHash := []byte(nil) // from genesis
+	startSlot := uint64(0)
+	startBlockNum := uint64(math.MaxUint64)
+	addressesOfInterest := []string{}
+
+	// for test net
+	address = "preprod-node.play.dev.cardano.org:3001"
+	networkMagic = 1
+
+	// for main net
+	address = "backbone.cardano-mainnet.iohk.io:3001"
+	networkMagic = uint32(764824073)
+
+	startBlockHash, _ = hex.DecodeString("5d9435abf2a829142aaae08720afa05980efaa6ad58e47ebd4cffadc2f3c45d8")
+	startSlot = uint64(76592549)
+	startBlockNum = 7999980
+	addressesOfInterest = []string{
+		"addr1v9kganeshgdqyhwnyn9stxxgl7r4y2ejfyqjn88n7ncapvs4sugsd",
+	}
+
+	logger, err := logger.NewLogger(logger.LoggerConfig{
+		LogLevel:      hclog.Debug,
+		JSONLogFormat: false,
+		AppendFile:    true,
+		LogFilePath:   "logs/cardano_indexer",
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	dbs, err := db.NewDatabaseInit("", "burek.db")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		logger.Error("Open database failed", "err", err)
+		os.Exit(1)
+	}
+
+	defer dbs.Close()
+
+	confirmedBlockHandler := func(block *indexer.FullBlock) error {
+		logger.Info("Confirmed block", "block", block)
+
+		blocks, err := dbs.GetUnprocessedConfirmedBlocks(0)
+		if err != nil {
+			return err
+		}
+
+		for _, b := range blocks {
+			logger.Info("Block has been processed", "block", b)
+		}
+
+		if err := dbs.MarkConfirmedBlocksProcessed(blocks); err != nil {
+			return err
+		}
+
+		return nil
+	}
+
+	indexerConfig := &indexer.BlockIndexerConfig{
+		StartingBlockPoint: &indexer.BlockPoint{
+			BlockSlot:   startSlot,
+			BlockHash:   startBlockHash,
+			BlockNumber: startBlockNum,
+		},
+		AddressCheck:           indexer.AddressCheckAll,
+		ConfirmationBlockCount: 10,
+		AddressesOfInterest:    addressesOfInterest,
+		SoftDeleteUtxo:         true,
+	}
+	syncerConfig := &indexer.BlockSyncerConfig{
+		NetworkMagic:   networkMagic,
+		NodeAddress:    address,
+		RestartOnError: true,
+		RestartDelay:   time.Second * 2,
+		KeepAlive:      true,
+	}
+
+	blockIndexer := indexer.NewBlockIndexer(indexerConfig, confirmedBlockHandler, dbs, logger.Named("block_indexer"))
+
+	blockSyncer := indexer.NewBlockSyncer(syncerConfig, blockIndexer, logger.Named("block_syncer"))
+	defer blockSyncer.Close()
+
+	err = blockSyncer.Sync()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		logger.Error("Start syncing failed", "err", err)
+		os.Exit(1)
+	}
+
+	signalChannel := make(chan os.Signal, 1)
+	// Notify the signalChannel when the interrupt signal is received (Ctrl+C)
+	signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case <-signalChannel:
+	case <-blockSyncer.ErrorCh():
+	}
+}
