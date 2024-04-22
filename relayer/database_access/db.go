a68d376a55e637a4d70e3dd26528becfc97daee5
Igor Crevar <crewce@gmail.com>
2024-04-22 17:42:52 +0200

    small fix

--- relayer/database_access/db.go
+++ relayer/database_access/db.go
@@ -2,13 +2,14 @@ package database_access
 
 import (
 	"fmt"
+	"path"
 
 	"github.com/Ethernal-Tech/apex-bridge/common"
 	"github.com/Ethernal-Tech/apex-bridge/relayer/core"
 )
 
 func NewDatabase(filePath string) (core.Database, error) {
-	if err := common.CreateDirectoryIfNotExists(filePath, 0770); err != nil {
+	if err := common.CreateDirectoryIfNotExists(path.Dir(filePath), 0770); err != nil {
 		return nil, fmt.Errorf("failed to create directory for relayer database: %w", err)
 	}
 
