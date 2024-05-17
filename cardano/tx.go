58e42a593b29ac6dd302b5fa34ade9fd0622ca2e
Igor Crevar <crewce@gmail.com>
2024-05-17 17:03:35 +0200

    remove unused struct

--- cardano/tx.go
+++ cardano/tx.go
@@ -1,13 +1,10 @@
 package cardanotx
 
 import (
-	"encoding/hex"
-	"encoding/json"
 	"math/big"
 
 	"github.com/Ethernal-Tech/apex-bridge/common"
 	cardanowallet "github.com/Ethernal-Tech/cardano-infrastructure/wallet"
-	"github.com/fxamacker/cbor/v2"
 )
 
 // CreateTx creates tx and returns cbor of raw transaction data, tx hash and error
@@ -91,46 +88,6 @@ func CreateTxWitness(txHash string, key cardanowallet.ISigner) ([]byte, error) {
 // AssembleTxWitnesses assembles all witnesses in final cbor of signed tx
 func AssembleTxWitnesses(txRaw []byte, witnesses [][]byte) ([]byte, error) {
 	return cardanowallet.AssembleTxWitnesses(txRaw, witnesses)
-}
-
-type SigningKey struct {
-	private []byte
-	public  []byte
-}
-
-func NewSigningKey(s string) SigningKey {
-	private := decodeCbor(s)
-
-	return SigningKey{
-		private: private,
-		public:  cardanowallet.GetVerificationKeyFromSigningKey(private),
-	}
-}
-
-func (sk SigningKey) GetSigningKey() []byte {
-	return sk.private
-}
-
-func (sk SigningKey) GetVerificationKey() []byte {
-	return sk.public
-}
-
-func decodeCbor(s string) (r []byte) {
-	b, _ := hex.DecodeString(s)
-	_ = cbor.Unmarshal(b, &r)
-
-	return r
-}
-
-func CreateMetaData(v *big.Int) ([]byte, error) {
-	metadata := map[string]interface{}{
-		"0": map[string]interface{}{
-			"type":  "multi",
-			"value": v.String(),
-		},
-	}
-
-	return json.Marshal(metadata)
 }
 
 func CreateBatchMetaData(v *big.Int) ([]byte, error) {
