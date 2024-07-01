5bceb79441f7ad3bd0bb54e34185c3b5b92799f2
Dejan-Nedic <dejan.nedic@ethernal.tech>
2024-07-01 11:00:47 +0200

    Initial implementation of eth oracle (#97)
    * Initial implementation of eth oracle
    
    * Initial impelemntaion of NewEthOracle

--- eth_oracle/oracle/oracle.go
+++ eth_oracle/oracle/oracle.go
@@ -1,1 +1,73 @@
 package oracle
+
+import (
+	"context"
+
+	"github.com/Ethernal-Tech/apex-bridge/oracle/core"
+	"github.com/hashicorp/go-hclog"
+)
+
+const (
+	MainComponentName = "eth_oracle"
+)
+
+type OracleImpl struct {
+	ctx       context.Context
+	appConfig *core.AppConfig
+	logger    hclog.Logger
+
+	errorCh chan error
+}
+
+var _ core.Oracle = (*OracleImpl)(nil)
+
+func NewEthOracle(
+	ctx context.Context,
+	appConfig *core.AppConfig,
+	logger hclog.Logger,
+) (*OracleImpl, error) {
+	return &OracleImpl{
+		ctx:       ctx,
+		appConfig: appConfig,
+		logger:    logger,
+	}, nil
+}
+
+func (o *OracleImpl) Start() error {
+	o.logger.Debug("Starting EthOracle")
+
+	o.errorCh = make(chan error, 1)
+	go o.errorHandler()
+
+	o.logger.Debug("Started EthOracle")
+
+	return nil
+}
+
+func (o *OracleImpl) Dispose() error {
+	close(o.errorCh)
+
+	return nil
+}
+
+func (o *OracleImpl) ErrorCh() <-chan error {
+	return o.errorCh
+}
+
+type ErrorOrigin struct {
+	err    error
+	origin string
+}
+
+func (o *OracleImpl) errorHandler() {
+	agg := make(chan ErrorOrigin)
+	defer close(agg)
+
+	select {
+	case errorOrigin := <-agg:
+		o.logger.Error("critical error", "origin", errorOrigin.origin, "err", errorOrigin.err)
+		o.errorCh <- errorOrigin.err
+	case <-o.ctx.Done():
+	}
+	o.logger.Debug("Exiting eth_oracle error handler")
+}
