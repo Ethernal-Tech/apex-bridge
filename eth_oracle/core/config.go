c80f3900fa0f6de87c090a9924c1197506cd4c4f
Dejan-Nedic <dejan.nedic@ethernal.tech>
2024-07-02 14:37:41 +0200

    Add eth oracle configs (#98)
    * Add eth oracle configs
    
    * APEX-242 init and start eth oracle from validatorcomonents
    
    * Fix error handling for oracle and eth_oracle
    
    * Fix review findings

--- eth_oracle/core/config.go
+++ eth_oracle/core/config.go
@@ -1,7 +1,7 @@
 package core
 
 import (
-	"github.com/Ethernal-Tech/cardano-infrastructure/logger"
+	oracleCore "github.com/Ethernal-Tech/apex-bridge/oracle/core"
 )
 
 type BridgingAddresses struct {
@@ -18,32 +18,13 @@ type SubmitConfig struct {
 	ConfirmedBlocksSubmitTime int `json:"confirmedBlocksSubmitTime"`
 }
 
-type BridgeConfig struct {
-	NodeURL              string       `json:"nodeUrl"`
-	DynamicTx            bool         `json:"dynamicTx"`
-	SmartContractAddress string       `json:"scAddress"`
-	SubmitConfig         SubmitConfig `json:"submitConfig"`
-}
-
-type AppSettings struct {
-	Logger  logger.LoggerConfig `json:"logger"`
-	DbsPath string              `json:"dbsPath"`
-}
-
-type BridgingSettings struct {
-	MinFeeForBridging              uint64 `json:"minFeeForBridging"`
-	UtxoMinValue                   uint64 `json:"utxoMinValue"`
-	MaxReceiversPerBridgingRequest int    `json:"maxReceiversPerBridgingRequest"`
-	MaxBridgingClaimsToGroup       int    `json:"maxBridgingClaimsToGroup"`
-}
-
 type AppConfig struct {
-	ValidatorDataDir    string                     `json:"validatorDataDir"`
-	ValidatorConfigPath string                     `json:"validatorConfigPath"`
-	EthChains           map[string]*EthChainConfig `json:"ethChains"`
-	Bridge              BridgeConfig               `json:"bridge"`
-	Settings            AppSettings                `json:"appSettings"`
-	BridgingSettings    BridgingSettings           `json:"bridgingSettings"`
+	ValidatorDataDir    string                      `json:"validatorDataDir"`
+	ValidatorConfigPath string                      `json:"validatorConfigPath"`
+	EthChains           map[string]*EthChainConfig  `json:"ethChains"`
+	Bridge              oracleCore.BridgeConfig     `json:"bridge"`
+	Settings            oracleCore.AppSettings      `json:"appSettings"`
+	BridgingSettings    oracleCore.BridgingSettings `json:"bridgingSettings"`
 }
 
 func (appConfig *AppConfig) FillOut() {
--- validatorcomponents/validatorcomponents/validatorcomponents.go
+++ validatorcomponents/validatorcomponents/validatorcomponents.go
@@ -20,7 +20,10 @@ import (
 
 	batchermanager "github.com/Ethernal-Tech/apex-bridge/batcher/batcher_manager"
 	batcherCore "github.com/Ethernal-Tech/apex-bridge/batcher/core"
+	ethOracle "github.com/Ethernal-Tech/apex-bridge/eth_oracle/oracle"
+
 	oracleCore "github.com/Ethernal-Tech/apex-bridge/oracle/core"
+
 	"github.com/Ethernal-Tech/apex-bridge/oracle/oracle"
 )
 
@@ -34,11 +37,13 @@ type ValidatorComponentsImpl struct {
 	shouldRunAPI    bool
 	db              core.Database
 	oracle          oracleCore.Oracle
+	ethOracle       oracleCore.Oracle
 	batcherManager  batcherCore.BatcherManager
 	relayerImitator core.RelayerImitator
 	api             core.API
 	telemetry       *telemetry.Telemetry
 	logger          hclog.Logger
+	errorCh         chan error
 }
 
 var _ core.ValidatorComponents = (*ValidatorComponentsImpl)(nil)
@@ -64,7 +69,7 @@ func NewValidatorComponents(
 		return nil, fmt.Errorf("failed to create BridgingRequestStateManager. err: %w", err)
 	}
 
-	oracleConfig, batcherConfig := appConfig.SeparateConfigs()
+	oracleConfig, ethOracleConfig, batcherConfig := appConfig.SeparateConfigs()
 
 	err = fixChainsAndAddresses(
 		ctx, oracleConfig,
@@ -92,6 +97,11 @@ func NewValidatorComponents(
 	oracle, err := oracle.NewOracle(ctx, oracleConfig, indexerDbs, bridgingRequestStateManager, logger.Named("oracle"))
 	if err != nil {
 		return nil, fmt.Errorf("failed to create oracle. err %w", err)
+	}
+
+	ethOracle, err := ethOracle.NewEthOracle(ctx, ethOracleConfig, logger.Named("eth_oracle"))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create eth_oracle. err %w", err)
 	}
 
 	batcherManager, err := batchermanager.NewBatcherManager(
@@ -132,6 +142,7 @@ func NewValidatorComponents(
 		shouldRunAPI:    shouldRunAPI,
 		db:              db,
 		oracle:          oracle,
+		ethOracle:       ethOracle,
 		batcherManager:  batcherManager,
 		relayerImitator: relayerImitator,
 		api:             apiObj,
@@ -153,6 +164,11 @@ func (v *ValidatorComponentsImpl) Start() error {
 		return fmt.Errorf("failed to start oracle. error: %w", err)
 	}
 
+	err = v.ethOracle.Start()
+	if err != nil {
+		return fmt.Errorf("failed to start eth_oracle. error: %w", err)
+	}
+
 	v.batcherManager.Start()
 
 	if v.shouldRunAPI {
@@ -160,6 +176,10 @@ func (v *ValidatorComponentsImpl) Start() error {
 	}
 
 	go v.relayerImitator.Start()
+
+	v.errorCh = make(chan error, 1)
+
+	go v.errorHandler()
 
 	v.logger.Debug("Started ValidatorComponents")
 
@@ -174,6 +194,11 @@ func (v *ValidatorComponentsImpl) Dispose() error {
 	if err := v.oracle.Dispose(); err != nil {
 		v.logger.Error("error while disposing oracle", "err", err)
 		errs = append(errs, fmt.Errorf("error while disposing oracle. err: %w", err))
+	}
+
+	if err := v.ethOracle.Dispose(); err != nil {
+		v.logger.Error("error while disposing eth_oracle", "err", err)
+		errs = append(errs, fmt.Errorf("error while disposing eth_oracle. err: %w", err))
 	}
 
 	if v.shouldRunAPI {
@@ -193,6 +218,8 @@ func (v *ValidatorComponentsImpl) Dispose() error {
 		errs = append(errs, fmt.Errorf("failed to close telemetry. err: %w", err))
 	}
 
+	close(v.errorCh)
+
 	if len(errs) > 0 {
 		return fmt.Errorf("errors while disposing validatorcomponents. errors: %w", errors.Join(errs...))
 	}
@@ -203,7 +230,23 @@ func (v *ValidatorComponentsImpl) Dispose() error {
 }
 
 func (v *ValidatorComponentsImpl) ErrorCh() <-chan error {
-	return v.oracle.ErrorCh()
+	return v.errorCh
+}
+
+func (v *ValidatorComponentsImpl) errorHandler() {
+outsideloop:
+	for {
+		select {
+		case err := <-v.oracle.ErrorCh():
+			v.errorCh <- err
+		case err := <-v.ethOracle.ErrorCh():
+			v.errorCh <- err
+		case <-v.ctx.Done():
+			break outsideloop
+		}
+	}
+
+	v.logger.Debug("Exiting validatorcomponents error handler")
 }
 
 func fixChainsAndAddresses(
