4a92ae55f1e3473df604a3908f3e17a2c1bcf152
babinskizoltan <babinskizoltan@gmail.com>
2024-07-05 09:00:55 +0200

    eth_oracle db and a few more fixes (#102)

--- eth_oracle/oracle/oracle.go
+++ eth_oracle/oracle/oracle.go
@@ -2,8 +2,18 @@ package oracle
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"path"
 
+	"github.com/Ethernal-Tech/apex-bridge/common"
+	"github.com/Ethernal-Tech/apex-bridge/eth_oracle/core"
+	databaseaccess "github.com/Ethernal-Tech/apex-bridge/eth_oracle/database_access"
+	"github.com/Ethernal-Tech/apex-bridge/eth_oracle/processor"
+	failedtxprocessors "github.com/Ethernal-Tech/apex-bridge/eth_oracle/processor/failed_tx_processors"
+	txprocessors "github.com/Ethernal-Tech/apex-bridge/eth_oracle/processor/tx_processors"
 	oracleCore "github.com/Ethernal-Tech/apex-bridge/oracle/core"
+	eventTrackerStore "github.com/Ethernal-Tech/blockchain-event-tracker/store"
 	"github.com/hashicorp/go-hclog"
 )
 
@@ -12,9 +22,11 @@ const (
 )
 
 type OracleImpl struct {
-	ctx       context.Context
-	appConfig *oracleCore.AppConfig
-	logger    hclog.Logger
+	ctx             context.Context
+	appConfig       *oracleCore.AppConfig
+	ethTxsProcessor core.EthTxsProcessor
+	db              core.Database
+	logger          hclog.Logger
 
 	errorCh chan error
 }
@@ -24,17 +36,45 @@ var _ oracleCore.Oracle = (*OracleImpl)(nil)
 func NewEthOracle(
 	ctx context.Context,
 	appConfig *oracleCore.AppConfig,
+	bridgeDataFetcher oracleCore.BridgeDataFetcher,
+	bridgeSubmitter oracleCore.BridgeSubmitter,
+	indexerDbs map[string]eventTrackerStore.EventTrackerStore,
+	bridgingRequestStateUpdater common.BridgingRequestStateUpdater,
 	logger hclog.Logger,
 ) (*OracleImpl, error) {
+	db, err := databaseaccess.NewDatabase(path.Join(appConfig.Settings.DbsPath, MainComponentName+".db"))
+	if err != nil {
+		return nil, fmt.Errorf("failed to open eth oracle database: %w", err)
+	}
+
+	txProcessors := []core.EthTxProcessor{
+		txprocessors.NewEthBatchExecutedProcessor(logger),
+		txprocessors.NewEthBridgingRequestedProcessor(logger),
+		// tx_processors.NewRefundExecutedProcessor(logger),
+	}
+
+	failedTxProcessors := []core.EthTxFailedProcessor{
+		failedtxprocessors.NewEthBatchExecutionFailedProcessor(logger),
+		// failed_tx_processors.NewRefundExecutionFailedProcessor(logger),
+	}
+
+	ethTxsProcessor := processor.NewEthTxsProcessor(
+		ctx, appConfig, db, txProcessors, failedTxProcessors, bridgeSubmitter,
+		indexerDbs, bridgingRequestStateUpdater, logger.Named("eth_txs_processor"))
+
 	return &OracleImpl{
-		ctx:       ctx,
-		appConfig: appConfig,
-		logger:    logger,
+		ctx:             ctx,
+		appConfig:       appConfig,
+		ethTxsProcessor: ethTxsProcessor,
+		db:              db,
+		logger:          logger,
 	}, nil
 }
 
 func (o *OracleImpl) Start() error {
 	o.logger.Debug("Starting EthOracle")
+
+	go o.ethTxsProcessor.Start()
 
 	o.errorCh = make(chan error, 1)
 	go o.errorHandler()
@@ -45,7 +85,19 @@ func (o *OracleImpl) Start() error {
 }
 
 func (o *OracleImpl) Dispose() error {
+	errs := make([]error, 0)
+
+	err := o.db.Close()
+	if err != nil {
+		o.logger.Error("Failed to close eth_oracle db", "err", err)
+		errs = append(errs, fmt.Errorf("failed to close eth_oracle db. err %w", err))
+	}
+
 	close(o.errorCh)
+
+	if len(errs) > 0 {
+		return fmt.Errorf("errors while disposing eth_oracle. errors: %w", errors.Join(errs...))
+	}
 
 	return nil
 }
