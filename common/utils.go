72a236d4af7eb1343408e4d11adf7a14d78c1b25
Dejan-Nedic <dejan.nedic@ethernal.tech>
2024-08-06 11:36:05 +0200

    Add retry mechanism to start of api server. (#127)

--- common/utils.go
+++ common/utils.go
@@ -95,3 +95,34 @@ func SafeSubtract(a, b, def uint64) uint64 {
 func MustHashToBytes32(hash string) (res [32]byte) {
 	return indexer.NewHashFromHexString(hash)
 }
+
+type IsRecoverableErrorFn func(err error) bool
+
+var ErrExecutionTimeout = errors.New("timeout while trying to execute with retry")
+
+// ExecuteWithRetry attempts to execute the provided executeFn function multiple times
+// if the call fails with a recoverable error. It retries up to numRetries times.
+func ExecuteWithRetry(ctx context.Context,
+	numRetries int, waitTime time.Duration,
+	executeFn func(ctx context.Context) (bool, error),
+	isRecoverableError ...IsRecoverableErrorFn,
+) error {
+	for count := 0; count < numRetries; count++ {
+		stop, err := executeFn(ctx)
+		if err != nil {
+			if len(isRecoverableError) == 0 || !isRecoverableError[0](err) {
+				return err
+			}
+		} else if stop {
+			return nil
+		}
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-time.After(waitTime):
+		}
+	}
+
+	return ErrExecutionTimeout
+}
