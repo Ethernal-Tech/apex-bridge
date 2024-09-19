1c0d842ce2eb058d0226111ff5cfd7b8af41f247
Igor Crevar <crewce@gmail.com>
2024-09-19 11:54:12 +0200

    CompileAndLoadContract func

--- eth/contracts/artifact.go
+++ eth/contracts/artifact.go
@@ -2,11 +2,13 @@ package ethcontracts
 
 import (
 	"bytes"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
 	"strings"
 
 	"github.com/Ethernal-Tech/apex-bridge/common"
@@ -99,31 +101,10 @@ func LoadArtifacts(directory string, names ...string) (map[string]*Artifact, err
 }
 
 // CloneAndBuildContracts clones and builds smart contracts
+// Note: git and npm must be in the path, repository must use hardhat
 func CloneAndBuildContracts(
 	dir, repositoryURL, repositoryName, artifactsDirName, branchName string,
 ) (string, error) {
-	executeCLICommand := func(binary string, args []string, workingDir string) (string, error) {
-		var (
-			stdErrBuffer bytes.Buffer
-			stdOutBuffer bytes.Buffer
-		)
-
-		cmd := exec.Command(binary, args...)
-		cmd.Stderr = &stdErrBuffer
-		cmd.Stdout = &stdOutBuffer
-		cmd.Dir = workingDir
-
-		err := cmd.Run()
-
-		if stdErrBuffer.Len() > 0 {
-			return "", fmt.Errorf("error while executing command: %s", stdErrBuffer.String())
-		} else if err != nil {
-			return "", err
-		}
-
-		return stdOutBuffer.String(), nil
-	}
-
 	if _, err := executeCLICommand(
 		"git", []string{"clone", "--progress", repositoryURL}, dir); err != nil {
 		// git clone writes to stderror, check if messages are ok...
@@ -147,4 +128,81 @@ func CloneAndBuildContracts(
 	}
 
 	return filepath.Join(dir, artifactsDirName), nil
+}
+
+// CompileContract compiles contract with solcjs and then loads artifact. DeployedBytecode will be nil
+// Note: solcjs must be in the path
+func CompileAndLoadContract(contractFilePath string, includePath string) (*Artifact, error) {
+	workingPath, err := os.MkdirTemp("", "compile-and-load-contract")
+	if err != nil {
+		return nil, err
+	}
+
+	defer os.RemoveAll(workingPath)
+
+	fileName := filepath.Base(contractFilePath)
+	directory := filepath.Dir(contractFilePath)
+	fileNameWithoutExt := strings.TrimSuffix(fileName, filepath.Ext(fileName))
+
+	params := []string{
+		"--optimize", "--bin", "--abi", "-o", workingPath, fileName,
+	}
+	if includePath != "" {
+		params = append(params, "--include-path", includePath, "--base-path", ".")
+	}
+
+	if _, err := executeCLICommand("solcjs", params, directory); err != nil {
+		return nil, err
+	}
+
+	parsedFileName := regexp.MustCompile(`[^a-zA-Z0-9]+`).ReplaceAllString(fileName, "_")
+	binFilePath := filepath.Join(workingPath, fmt.Sprintf("%s_%s.bin", parsedFileName, fileNameWithoutExt))
+	abiFilePath := filepath.Join(workingPath, fmt.Sprintf("%s_%s.abi", parsedFileName, fileNameWithoutExt))
+
+	jsonRaw, err := os.ReadFile(abiFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load generated abi file: %s", abiFilePath)
+	}
+
+	hexBin, err := os.ReadFile(binFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load generated bin file: %s", binFilePath)
+	}
+
+	abi := new(abi.ABI)
+	if err := json.Unmarshal(jsonRaw, &abi); err != nil {
+		return nil, err
+	}
+
+	bytecode, err := hex.DecodeString(string(hexBin))
+	if err != nil {
+		return nil, err
+	}
+
+	return &Artifact{
+		Abi:      abi,
+		Bytecode: bytecode,
+	}, nil
+}
+
+func executeCLICommand(binary string, args []string, workingDir string) (string, error) {
+	var (
+		stdErrBuffer bytes.Buffer
+		stdOutBuffer bytes.Buffer
+	)
+
+	cmd := exec.Command(binary, args...)
+	cmd.Stderr = &stdErrBuffer
+	cmd.Stdout = &stdOutBuffer
+	cmd.Dir = workingDir
+
+	err := cmd.Run()
+
+	if stdErrBuffer.Len() > 0 {
+		return "", fmt.Errorf("error while executing command: %s", stdErrBuffer.String())
+	} else if err != nil {
+		return "", err
+	}
+
+	return stdOutBuffer.String(), nil
 }
