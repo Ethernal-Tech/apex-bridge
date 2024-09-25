0e9da8a42dd1f3b21f79e55855de2e8a4de4f898
Igor Crevar <crewce@gmail.com>
2024-09-25 18:15:36 +0200

    small

--- relayer/relayer/evm_chain_operations.go
+++ relayer/relayer/evm_chain_operations.go
@@ -74,7 +74,7 @@ func (cco *EVMChainOperations) SendTx(
 	signature, _ := signatures.Aggregate().Marshal() // error is always nil
 
 	cco.logger.Info("Submitting deposit transaction",
-		"signature", hex.EncodeToString(signature), "bitmap", smartContractData.Bitmap)
+		"signature", hex.EncodeToString(signature), "bitmap", common.NewBitmap(smartContractData.Bitmap))
 
 	return cco.evmSmartContract.Deposit(ctx, signature, smartContractData.Bitmap, smartContractData.RawTransaction)
 }
