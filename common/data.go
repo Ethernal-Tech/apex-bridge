b0950b995b24484d2cf3f5e14b18ab81ff00efcf
Zoltan Babinski <babinskizoltan@gmail.com>
2025-01-10 15:01:45 +0100

    updated infra. switched to creating bridging tx from infra. fixed some bugs

--- common/data.go
+++ common/data.go
@@ -11,8 +11,10 @@ const (
 
 	EthZeroAddr = "0x0000000000000000000000000000000000000000"
 
-	MinFeeForBridgingDefault = uint64(1_000_010)
-	MinUtxoAmountDefault     = uint64(1_000_000)
+	MinFeeForBridgingDefault      = uint64(1_000_010)
+	MinUtxoAmountDefault          = uint64(1_000_000)
+	PotentialFeeDefault           = 250_000
+	MaxInputsPerBridgingTxDefault = 16
 
 	ReactorMode VCRunMode = "reactor"
 	SkylineMode VCRunMode = "skyline"
