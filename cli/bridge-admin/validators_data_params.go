0155285f67e47e70f29e99b64fd72c403139c59d
dulicd <63311669+dulicd@users.noreply.github.com>
2025-04-01 08:48:33 +0200

    Admin cli commands (#251)
    * Get validators data command
    
    * readme
    
    * Set up new command structure and connect to indexer db
    
    * Use FixChainsAndAddresses to FillOut config
    
    * code cleanup (linter)
    
    * Get balances for cardano chains from DB
    
    * Don't call SC and print balance for nexus
    
    * Retrieve utxos via Ogmios
    
    * Added command in readme
    
    * new error message
    
    * Print number of filtered utxos
    
    * fix
    
    * Format validators data result
    
    * Resolve comments in PR
    
    * code cleanup (lint)
    
    * error messages

---
+++ cli/bridge-admin/validators_data_params.go
@@ -0,0 +1,199 @@
+package clibridgeadmin
+
+import (
+	"encoding/hex"
+	"fmt"
+	"os"
+
+	cardanotx "github.com/Ethernal-Tech/apex-bridge/cardano"
+	"github.com/Ethernal-Tech/apex-bridge/common"
+	"github.com/Ethernal-Tech/apex-bridge/contractbinding"
+	ethtxhelper "github.com/Ethernal-Tech/apex-bridge/eth/txhelper"
+	vcCore "github.com/Ethernal-Tech/apex-bridge/validatorcomponents/core"
+	"github.com/Ethernal-Tech/bn256"
+	"github.com/Ethernal-Tech/cardano-infrastructure/wallet"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	ethcommon "github.com/ethereum/go-ethereum/common"
+	"github.com/spf13/cobra"
+)
+
+const (
+	configFlag = "config"
+
+	configFlagDesc = "path to config json file"
+)
+
+type ValidatorChainData = contractbinding.IBridgeStructsValidatorChainData
+
+type validatorsDataParams struct {
+	bridgeNodeURL string
+	bridgeSCAddr  string
+	config        string
+}
+
+func (v *validatorsDataParams) ValidateFlags() error {
+	if !common.IsValidHTTPURL(v.bridgeNodeURL) {
+		return fmt.Errorf("invalid --%s flag", bridgeNodeURLFlag)
+	}
+
+	if !ethcommon.IsHexAddress(v.bridgeSCAddr) {
+		return fmt.Errorf("invalid --%s flag", bridgeSCAddrFlag)
+	}
+
+	if v.config == "" {
+		return fmt.Errorf("--%s flag not specified", configFlag)
+	}
+
+	if _, err := os.Stat(v.config); err != nil {
+		if os.IsNotExist(err) {
+			return fmt.Errorf("config file does not exist: %s", v.config)
+		}
+
+		return fmt.Errorf("failed to check config file: %s. err: %w", v.config, err)
+	}
+
+	return nil
+}
+
+func (v *validatorsDataParams) RegisterFlags(cmd *cobra.Command) {
+	cmd.Flags().StringVar(
+		&v.bridgeNodeURL,
+		bridgeNodeURLFlag,
+		"",
+		bridgeNodeURLFlagDesc,
+	)
+
+	cmd.Flags().StringVar(
+		&v.bridgeSCAddr,
+		bridgeSCAddrFlag,
+		apexBridgeScAddress.String(),
+		bridgeSCAddrFlagDesc,
+	)
+
+	cmd.Flags().StringVar(
+		&v.config,
+		configFlag,
+		"",
+		configFlagDesc,
+	)
+}
+
+func (v *validatorsDataParams) Execute(outputter common.OutputFormatter) (common.ICommandResult, error) {
+	txHelper, err := ethtxhelper.NewEThTxHelper(ethtxhelper.WithNodeURL(v.bridgeNodeURL))
+	if err != nil {
+		return nil, err
+	}
+
+	contract, err := contractbinding.NewBridgeContract(common.HexToAddress(v.bridgeSCAddr), txHelper.GetClient())
+	if err != nil {
+		return nil, err
+	}
+
+	config, err := common.LoadConfig[vcCore.AppConfig](v.config, "")
+	if err != nil {
+		return nil, err
+	}
+
+	allRegisteredChains, err := contract.GetAllRegisteredChains(&bind.CallOpts{})
+	if err != nil {
+		return nil, err
+	}
+
+	for _, regChain := range allRegisteredChains {
+		chainID := common.ToStrChainID(regChain.Id)
+
+		switch regChain.ChainType {
+		case common.ChainTypeCardano:
+			chainConfig, exists := config.CardanoChains[chainID]
+			if !exists {
+				return nil, err
+			}
+
+			validatorsData, err := contract.GetValidatorsChainData(&bind.CallOpts{}, common.ToNumChainID(chainID))
+			if err != nil {
+				return nil, err
+			}
+
+			_, _ = outputter.Write([]byte(fmt.Sprintf("Validators data on %s chain: \n", chainID)))
+
+			err = printChainValidatorsDataInfo(chainID, validatorsData, outputter)
+			if err != nil {
+				return nil, err
+			}
+
+			multisigPolicyScript, multisigFeePolicyScript, err := cardanotx.GetPolicyScripts(validatorsData)
+			if err != nil {
+				return nil, err
+			}
+
+			multisigAddr, feeAddr, err := cardanotx.GetMultisigAddresses(
+				wallet.ResolveCardanoCliBinary(chainConfig.NetworkID), uint(chainConfig.NetworkMagic),
+				multisigPolicyScript, multisigFeePolicyScript)
+			if err != nil {
+				return nil, err
+			}
+
+			_, _ = outputter.Write([]byte(fmt.Sprintf("Addresses on %s chain (retrieved from validator data): \n", chainID)))
+			_, _ = outputter.Write([]byte(fmt.Sprintf("Multisig Address =  %s\n", multisigAddr)))
+			_, _ = outputter.Write([]byte(fmt.Sprintf("Fee Payer Address = %s\n", feeAddr)))
+
+			_, _ = outputter.Write([]byte(fmt.Sprintf("Addresses on %s chain (retrieved from registered chains): \n", chainID)))
+			_, _ = outputter.Write([]byte(fmt.Sprintf("Multisig Address =  %s\n", regChain.AddressMultisig)))
+			_, _ = outputter.Write([]byte(fmt.Sprintf("Fee Payer Address = %s\n", regChain.AddressFeePayer)))
+			outputter.WriteOutput()
+		case common.ChainTypeEVM:
+			validatorsData, err := contract.GetValidatorsChainData(&bind.CallOpts{}, common.ToNumChainID(chainID))
+			if err != nil {
+				return nil, err
+			}
+
+			_, _ = outputter.Write([]byte(fmt.Sprintf("Validators data on %s chain: \n", chainID)))
+
+			err = printChainValidatorsDataInfo(chainID, validatorsData, outputter)
+			if err != nil {
+				return nil, err
+			}
+
+			_, _ = outputter.Write([]byte(fmt.Sprintf("Addresses on %s chain (retrieved from registered chains): \n", chainID)))
+			_, _ = outputter.Write([]byte(fmt.Sprintf("Multisig Address =  %s\n", regChain.AddressMultisig)))
+			outputter.WriteOutput()
+
+		default:
+		}
+	}
+
+	return nil, nil
+}
+
+func printChainValidatorsDataInfo(
+	chainID string, data []ValidatorChainData, outputter common.OutputFormatter,
+) error {
+	for _, x := range data {
+		var formattedData string
+
+		switch chainID {
+		case common.ChainIDStrNexus:
+			pub, err := bn256.UnmarshalPublicKeyFromBigInt(x.Key)
+			if err != nil {
+				return err
+			}
+
+			formattedData = fmt.Sprintf("BLSKey=%s", hex.EncodeToString(pub.Marshal()))
+		default:
+			formattedData = fmt.Sprintf(
+				"MultisigKey=%s, FeeKey=%s",
+				hex.EncodeToString(wallet.PadKeyToSize(x.Key[0].Bytes())),
+				hex.EncodeToString(wallet.PadKeyToSize(x.Key[1].Bytes())),
+			)
+		}
+
+		_, _ = outputter.Write([]byte(formattedData))
+		outputter.WriteOutput()
+	}
+
+	return nil
+}
+
+var (
+	_ common.CliCommandExecutor = (*validatorsDataParams)(nil)
+)
