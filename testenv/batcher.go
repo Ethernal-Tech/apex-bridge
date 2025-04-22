0a3c77122edbbd9fa0c27cf5a3f93527e352e5f7
Igor Crevar <crewce@gmail.com>
2025-04-22 16:02:04 +0200

    cr fixes

--- testenv/batcher.go
+++ testenv/batcher.go
@@ -5,10 +5,7 @@ package testenv
 
 type TTLFormatterFunc func(ttl uint64, batchID uint64) uint64
 
-// getTTLFormatter returns formater for a test mode. By default it is just identity function
-// 1 - first batch will fail
-// 2 - first five batches will fail
-// 3 - First batch 5 bathces fail in "random" predetermined sequence
+// GetTTLFormatter returns a TTLFormatterFunc that always returns the provided TTL value unchanged.
 func GetTTLFormatter(testMode uint8) TTLFormatterFunc {
 	return func(ttl, batchID uint64) uint64 {
 		return ttl
--- testenv/batcher_testenv.go
+++ testenv/batcher_testenv.go
@@ -5,7 +5,7 @@ package testenv
 
 type TTLFormatterFunc func(ttl uint64, batchID uint64) uint64
 
-// getTTLFormatter returns formater for a test mode. By default it is just identity function
+// GetTTLFormatter returns formater for a test mode. By default it is just identity function
 // 1 - first batch will fail
 // 2 - first five batches will fail
 // 3 - First batch 5 bathces fail in "random" predetermined sequence
