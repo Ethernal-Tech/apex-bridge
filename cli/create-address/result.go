826913885d64c301503a7f93d2ec91a920f115f3
Igor Crevar <crewce@gmail.com>
2025-06-18 15:32:40 +0200

    AD-698 apex-bridge - Add a policy.json flag to mint command (#305)

--- cli/create-address/result.go
+++ cli/create-address/result.go
@@ -6,10 +6,13 @@ import (
 
 	cardanotx "github.com/Ethernal-Tech/apex-bridge/cardano"
 	"github.com/Ethernal-Tech/apex-bridge/common"
+	"github.com/Ethernal-Tech/cardano-infrastructure/wallet"
 )
 
 type CmdResult struct {
 	cardanotx.ApexAddresses
+	PolicyScripts     cardanotx.ApexPolicyScripts
+	ShowPolicyScripts bool
 }
 
 func (r CmdResult) GetOutput() string {
@@ -17,18 +20,41 @@ func (r CmdResult) GetOutput() string {
 
 	args := []string{
 		fmt.Sprintf("Multisig Address|%s", r.Multisig.Payment),
-		fmt.Sprintf("Fee Payer Address|%s", r.Fee.Payment),
 	}
 
 	if r.Multisig.Stake != "" {
 		args = append(args, fmt.Sprintf("Multisig Stake Address|%s", r.Multisig.Stake))
 	}
 
+	args = append(args, fmt.Sprintf("Fee Payer Address|%s", r.Fee.Payment))
+
 	if r.Fee.Stake != "" {
 		args = append(args, fmt.Sprintf("Fee Payer Stake Address|%s", r.Fee.Stake))
 	}
 
-	buffer.WriteString(common.FormatKV(args))
+	_, _ = buffer.WriteString(common.FormatKV(args))
+
+	if r.ShowPolicyScripts {
+		showPS := func(title string, ps wallet.IPolicyScript) {
+			if ps == nil {
+				return
+			}
+
+			bytes, err := ps.GetPolicyScriptJSON()
+			if err != nil {
+				_, _ = buffer.WriteString(fmt.Sprintf("\nFailed to generate %s: %s",
+					title, err.Error()))
+			} else {
+				_, _ = buffer.WriteString(fmt.Sprintf("\n%s:\n%s\n", title, bytes))
+			}
+		}
+
+		buffer.WriteString("\n")
+		showPS("Multisig Payment Policy Script", r.PolicyScripts.Multisig.Payment)
+		showPS("Multisig Stake Policy Script", r.PolicyScripts.Multisig.Stake)
+		showPS("Fee Payer Payment Policy Script", r.PolicyScripts.Fee.Payment)
+		showPS("Fee Payer Stake Policy Script", r.PolicyScripts.Fee.Stake)
+	}
 
 	return buffer.String()
 }
