6a203d6b6afc8cc4ade45f43ca85579f92398665
sara-ethernal <sara.nikolic@ethernal.tech>
2025-07-15 13:02:42 +0200

    AD-731_stake_delegation_cli_command

---
+++ cli/bridge-admin/stake_delegation_params.go
@@ -0,0 +1,163 @@
+package clibridgeadmin
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/Ethernal-Tech/apex-bridge/common"
+	"github.com/Ethernal-Tech/apex-bridge/contractbinding"
+	"github.com/Ethernal-Tech/apex-bridge/eth"
+	ethtxhelper "github.com/Ethernal-Tech/apex-bridge/eth/txhelper"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/spf13/cobra"
+)
+
+const (
+	stakePoolIDFlag   = "stake-pool"
+	bridgeAddrIdxFlag = "bridge-address-index"
+
+	stakePoolIDFlagDesc   = "identifier of the stake pool to delegate to"
+	bridgeAddrIdxFlagDesc = "index of the bridging address to be delegated"
+)
+
+type stakeDelParams struct {
+	chainID          string
+	bridgeAddrIdx    uint8
+	stakePoolId      string
+	bridgeNodeURL    string
+	bridgePrivateKey string
+	privateKeyConfig string
+}
+
+// ValidateFlags implements common.CliCommandValidator.
+func (params *stakeDelParams) ValidateFlags() error {
+	if !common.IsValidHTTPURL(params.bridgeNodeURL) {
+		return fmt.Errorf("invalid --%s flag", bridgeNodeURLFlag)
+	}
+
+	if params.chainID == "" {
+		return fmt.Errorf("--%s flag not specified", chainIDFlag)
+	}
+
+	if params.stakePoolId == "" {
+		return fmt.Errorf("--%s flag not specified", stakePoolIDFlag)
+	}
+
+	if params.bridgePrivateKey == "" && params.privateKeyConfig == "" {
+		return fmt.Errorf("specify at least one: --%s or --%s", privateKeyFlag, privateKeyConfigFlag)
+	}
+
+	return nil
+}
+
+// Execute implements common.CliCommandExecutor.
+func (params *stakeDelParams) Execute(outputter common.OutputFormatter) (common.ICommandResult, error) {
+	ctx := context.Background()
+	chainIDInt := common.ToNumChainID(params.chainID)
+
+	_, _ = outputter.Write([]byte("creating and sending transaction..."))
+	outputter.WriteOutput()
+
+	wallet, err := eth.GetEthWalletForBladeAdmin(false, params.bridgePrivateKey, params.privateKeyConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create bridge admin wallet: %w", err)
+	}
+
+	txHelper, err := ethtxhelper.NewEThTxHelper(ethtxhelper.WithNodeURL(params.bridgeNodeURL))
+	if err != nil {
+		return nil, err
+	}
+
+	contract, err := contractbinding.NewBridgeContract(
+		apexBridgeScAddress,
+		txHelper.GetClient())
+	if err != nil {
+		return nil, err
+	}
+
+	abi, err := contractbinding.BridgeContractMetaData.GetAbi()
+	if err != nil {
+		return nil, err
+	}
+
+	estimatedGas, _, err := txHelper.EstimateGas(
+		ctx, wallet.GetAddress(), apexBridgeScAddress, nil, gasLimitMultiplier, abi,
+		"delegateAddrToStakePool", chainIDInt, params.bridgeAddrIdx, params.stakePoolId)
+	if err != nil {
+		return nil, err
+	}
+
+	tx, err := txHelper.SendTx(
+		ctx, wallet, bind.TransactOpts{}, func(opts *bind.TransactOpts) (*types.Transaction, error) {
+			opts.GasLimit = estimatedGas
+
+			return contract.DelegateAddrToStakePool(opts, chainIDInt, params.bridgeAddrIdx, params.stakePoolId)
+		})
+	if err != nil {
+		return nil, err
+	}
+
+	_, _ = outputter.Write([]byte(fmt.Sprintf("transaction has been submitted: %s", tx.Hash())))
+	outputter.WriteOutput()
+
+	receipt, err := txHelper.WaitForReceipt(ctx, tx.Hash().String())
+	if err != nil {
+		return nil, err
+	} else if receipt.Status != types.ReceiptStatusSuccessful {
+		return nil, errors.New("transaction receipt status is unsuccessful")
+	}
+
+	return &chainTokenQuantityResult{}, err
+}
+
+func (params *stakeDelParams) RegisterFlags(cmd *cobra.Command) {
+	cmd.Flags().StringVar(
+		&params.chainID,
+		chainIDFlag,
+		"",
+		chainIDFlagDesc,
+	)
+
+	cmd.Flags().Uint8Var(
+		&params.bridgeAddrIdx,
+		bridgeAddrIdxFlag,
+		0,
+		bridgeAddrIdxFlagDesc,
+	)
+
+	cmd.Flags().StringVar(
+		&params.stakePoolId,
+		stakePoolIDFlag,
+		"",
+		stakePoolIDFlagDesc,
+	)
+
+	cmd.Flags().StringVar(
+		&params.bridgeNodeURL,
+		bridgeNodeURLFlag,
+		"",
+		bridgeNodeURLFlagDesc,
+	)
+
+	cmd.Flags().StringVar(
+		&params.bridgePrivateKey,
+		privateKeyFlag,
+		"",
+		bridgePrivateKeyFlagDesc,
+	)
+
+	cmd.Flags().StringVar(
+		&params.privateKeyConfig,
+		privateKeyConfigFlag,
+		"",
+		privateKeyConfigFlagDesc,
+	)
+
+	cmd.MarkFlagsMutuallyExclusive(privateKeyConfigFlag, privateKeyFlag)
+}
+
+var (
+	_ common.CliCommandExecutor = (*stakeDelParams)(nil)
+)
