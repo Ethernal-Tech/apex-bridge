51634cb50aa3079cf8c8826a8e36eeb1e4ab244e
Nikola Jovićević <nikola.jovicevic@ethernal.tech>
2026-04-23 15:45:22 +0200

    add error check on npm install command

--- eth/contracts/artifact.go
+++ eth/contracts/artifact.go
@@ -122,7 +122,11 @@ func CloneAndBuildContracts(
 	// do not listen for errors on following commands
 	_, _ = common.ExecuteCLICommand("git", []string{"checkout", branchName}, dir)
 	_, _ = common.ExecuteCLICommand("git", []string{"pull", "origin"}, dir)
-	_, _ = common.ExecuteCLICommand("npm", []string{"install"}, dir)
+
+	_, err := common.ExecuteCLICommand("npm", []string{"install"}, dir)
+	if err != nil {
+		return "", fmt.Errorf("failed to install npm dependencies: %w", err)
+	}
 
 	if _, err := common.ExecuteCLICommand("npx", []string{"hardhat", "compile"}, dir); err != nil {
 		if !(strings.Contains(err.Error(), HardhatNodeVersionErr1) &&
